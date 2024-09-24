a8a4bfad729776ad577579678b43d3fd598364c0
Christopher Lim <toffer.lim87@gmail.com>
2024-09-23 20:59:35 -0700

    - remove the wait

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const sequelize = require('./migrations/init');
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
 app.use('/api', userRoutes);
 app.use('/api', bookRoutes);
 
